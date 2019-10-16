9d8d47b04aa35ef7262096fb296c1f65d8beee3f
ashwinxkumar01 <ashwinxkumar01@gmail.com>
2019-10-16 16:11:50 -0700

    Adding Geofencing Class

---
+++ app/src/main/java/com/example/quiethours/GeoFencing.java
@@ -0,0 +1,33 @@
+package com.example.quiethours;
+
+import android.os.Bundle;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.libraries.places.api.model.Place;
+
+import java.util.ArrayList;
+
+public class GeoFencing {
+    public GeofencingClient geofencingClient;
+    public ArrayList<Geofence> geofenceList;
+
+    public void onCreate(Bundle savedInstanceState) {
+        geofencingClient = LocationServices.getGeofencingClient(this);
+    }
+
+    public void createGeofence(Place place){
+        geofenceList.add(new Geofence.Builder()
+                // Set the request ID of the geofence. This is a string to identify this
+                // geofence.
+                .setRequestId(place.getId())
+
+                .setCircularRegion(
+                        place.getLatLng().latitude,
+                        place.getLatLng().longitude,
+                        100
+
+                );
+    }
+}
